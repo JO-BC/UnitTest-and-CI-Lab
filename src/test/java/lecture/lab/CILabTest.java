d5b768782e4c9118c00790c6db4bcf7a349c1350
Joshua O <joshua.o@bellevuecollege.edu>
2022-06-10 12:44:44 -0700

    Implemented class and test cases

--- src/test/java/lecture/lab/CILabTest.java
+++ src/test/java/lecture/lab/CILabTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CILabTest {
 
@@ -22,13 +22,16 @@ public class CILabTest {
 
     @Test
     public void detectCapitalUseTest1() {
-       fail("Not yet implemented");
-
+       myString.setString("USA");
+       assertTrue(myString.detectCapitalUse());
     }
     @Test
     public void detectCapitalUseTest2() {
-        fail("Not yet implemented");
+       myString.setString("FlaG");
+       assertFalse(myString.detectCapitalUse());
     }
-
-
+    public void detectCapitalUseTest3() {
+    	myString.setString("softwareengineering");
+    	assertTrue(myString.detectCapitalUse());
+    }
 }
